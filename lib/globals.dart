876f2a6d902de3a908d530383907ff536f5c36a3
ncrnko666 <60021156+ncrnko666@users.noreply.github.com>
2021-02-01 19:07:17 +0100

    firebase

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,2 +1,6 @@
+import 'dart:io';
+
 String picPath = null;
 bool sentServer = true;
+
+File imageFile;
--- lib/main.dart
+++ lib/main.dart
@@ -3,42 +3,38 @@
 // username: flutter_ftp
 // password: 67IbyHP3PVF0
 // upload directory: photos
-
-
+// https://github.com/nenadcrnko/flutter_sftp_test
 
 // -------- import all packages --------
 import 'package:flutter/material.dart';
-import 'dart:io';
 import 'package:flutter/services.dart';
+import 'dart:io';
+import 'dart:convert';
 
 import 'package:camera/camera.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:ssh/ssh.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'globals.dart' as globals;
-
-
 
 // -------- application start point --------
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "SFTP Test App",
-        home: HomePage()
-    );
+    return MaterialApp(title: "SFTP Test App", home: HomePage());
   }
-
 }
 
 
 
 
 
+// -------------------------------------------------------------------
 // -------- home page --------
 class HomePage extends StatefulWidget {
   @override
@@ -46,90 +42,128 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  // -------- render home page with AppBar and Buttons (second button only if picture exists) --------
+  // -------- render home page with AppBar and Buttons  --------
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("SFTP Test App"),
-          backgroundColor: Colors.blue[900],
-          actions: <Widget>[
+      appBar: AppBar(
+        title: Text("SFTP Test App"),
+        backgroundColor: Colors.blue[900],
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.info),
+            onPressed: () {
+              _showInfo(
+                  context,
+                  'About the app',
+                  'SFTP Test App   Version 1.0\n\n'
+                      'Developed by Crnko\n\n'
+                      'Take the picture from the camera and send to server.');
+            },
+          ),
 
-            IconButton(
-              icon: Icon(Icons.info),
-              onPressed: () {
-                showInfo(
-                    context,
-                    'About the app',
-                    'SFTP Test App   Version 1.0\n\n'
-                        'Developed by Crnko\n\n'
-                        'Take the picture from the camera and send to server.'
-                  );
-              },
-            ),
+          // exit icon only on Android platform
+          Platform.isAndroid
+              ? IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+            },
+          )
+              : Container(),
+        ],
+      ),
+      body: ListView(children: <Widget>[
+        Column(children: <Widget>[
+          // show picture from camera or nothing on start
+          globals.picPath == null
+              ? Container()
+              : Image.file(File(globals.picPath)),
 
-            // exit icon only on Android platform
-            Platform.isAndroid ?
-            IconButton(
-              icon: Icon(Icons.exit_to_app),
-              onPressed: () {
-                SystemChannels.platform.invokeMethod('SystemNavigator.pop');
-              },
-            ): Container(),
-
-          ],
-        ),
-        body: Center(
-          child: Column(children: <Widget>[
-            // show picture from camera or nothing on start
-            globals.picPath == null
-                ? Container()
-                : Image.file(File(globals.picPath)),
-
-            SizedBox(height: 20),
-            ButtonTheme(
+          SizedBox(height: 20),
+          ButtonTheme(
             minWidth: 150.0,
             height: 40.0,
-              child: FlatButton(
-                child:
-                    Text("Take Picture", style: TextStyle(color: Colors.white)),
-                color: Colors.blue[700],
-                onPressed: () {
-                  _showCamera(context);
-                },
-              ),
+            child: FlatButton(
+              child:
+              Text("Show Firebase", style: TextStyle(color: Colors.white)),
+              color: Colors.blue[700],
+              onPressed: () {
+                _showFirebase(context);
+              },
             ),
-            SizedBox(height: 20),
-            globals.picPath != null
-                ? ButtonTheme(
-                    minWidth: 150.0,
-                    height: 40.0,
-                    child: FlatButton(
-                      child: Text("Send to Server",
-                          style: TextStyle(color: Colors.white)),
-                      color: Colors.blue[700],
-                      onPressed: () {
-                        _sendServer(context);
-                      },
-                    ),
-                  )
-                : Container(),
-          ]),
-        ));
+          ),
+
+          SizedBox(height: 20),
+          ButtonTheme(
+            minWidth: 150.0,
+            height: 40.0,
+            child: FlatButton(
+              child:
+              Text("Take Picture", style: TextStyle(color: Colors.white)),
+              color: Colors.blue[700],
+              onPressed: () {
+                _showCamera(context);
+              },
+            ),
+          ),
+
+          SizedBox(height: 20),
+          globals.picPath != null
+              ? ButtonTheme(
+            minWidth: 150.0,
+            height: 40.0,
+            child: FlatButton(
+              child: Text("Send to Server",
+                  style: TextStyle(color: Colors.white)),
+              color: Colors.blue[700],
+              onPressed: () {
+                _sendServer(context);
+              },
+            ),
+          )
+              : Container(),
+
+          SizedBox(height: 20),
+          globals.picPath != null
+              ? ButtonTheme(
+            minWidth: 150.0,
+            height: 40.0,
+            child: FlatButton(
+              child: Text("Send to Firebase",
+                  style: TextStyle(color: Colors.white)),
+              color: Colors.blue[700],
+              onPressed: () {
+                _sendFirebase(context);
+              },
+            ),
+          )
+              : Container(),
+        ]),
+      ]),
+    );
   }
 
   // -------- call Second Class for camera support --------
   void _showCamera(BuildContext context) async {
-
     final cameras = await availableCameras();
     final camera = cameras.first;
-    final result = await Navigator.push(context, MaterialPageRoute(builder: (context) => TakePicture(camera: camera)));
+    final result = await Navigator.push(context,
+        MaterialPageRoute(builder: (context) => TakePicture(camera: camera)));
 
     setState(() {
       globals.picPath = result;
+      globals.imageFile = File(globals.picPath);
     });
+  }
 
+  // -------- call Third Class for Firebase --------
+  void _showFirebase(BuildContext context) async {
+    final result = await Navigator.push(context,
+        MaterialPageRoute(builder: (context) => FireBasePicture()));
+
+    setState(() {
+    });
   }
 
   // -------- send file to Server if exists --------
@@ -149,38 +183,46 @@ class _HomePageState extends State<HomePage> {
       if (resultConnect.toString() == 'session_connected') {
         final resultUpload = await client.sftpUpload(
           path: globals.picPath,
-          toPath: "./photos",     // maybe wrong server path, some security problem or error in package:ssh/ssh.dart
+          toPath: "./photos",
           callback: (progress) async {
             print(progress);
           },
         );
-        if (resultUpload == 'upload_success')
-          globals.sentServer = true;
+        if (resultUpload == 'upload_success') globals.sentServer = true;
       }
       await client.disconnect();
-
     } catch (e) {
       print(e);
       globals.sentServer = false;
     }
 
-    if (globals.sentServer == true){
-      showInfo(context, 'Upload file', 'The file is uploaded to the server');
-    }
-    else {
-      showInfo(context, 'Upload file', 'Error during upload');
+    if (globals.sentServer == true) {
+      _showInfo(context, 'Upload file', 'The file is uploaded to the server');
+    } else {
+      _showInfo(context, 'Upload file', 'Error during upload');
     }
 
     setState(() {});
+  }
 
+  // -------- send file to Server if exists --------
+  void _sendFirebase(BuildContext context) async {
+    final file = File(globals.picPath);
+    final bytes = await file.readAsBytes();
+    String picdata = jsonEncode(bytes);
+    Firestore.instance.collection('dbphotos').document()
+        .setData({ 'name': globals.picPath, 'value': picdata });
+    _showInfo(context, 'Uploaded to Firebase', 'Collection of documents dbphotos');
   }
 
   // -------- general function for show dialog box --------
-  showInfo(BuildContext context, String title, String message) {
+  _showInfo(BuildContext context, String title, String message) {
     // set up the button
     Widget okButton = FlatButton(
       child: Text("Close"),
-      onPressed: () {Navigator.of(context).pop();},
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
     );
 
     // set up the AlertDialog
@@ -206,6 +248,7 @@ class _HomePageState extends State<HomePage> {
 
 
 
+// -------------------------------------------------------------------
 // -------- second class for capture picture --------
 class TakePicture extends StatefulWidget {
   final CameraDescription camera;
@@ -223,7 +266,8 @@ class _TakePictureState extends State<TakePicture> {
   @override
   void initState() {
     super.initState();
-    _cameraController = CameraController(widget.camera, ResolutionPreset.medium);
+    _cameraController =
+        CameraController(widget.camera, ResolutionPreset.medium);
     _initializeCameraControllerFuture = _cameraController.initialize();
   }
 
@@ -231,10 +275,10 @@ class _TakePictureState extends State<TakePicture> {
     try {
       await _initializeCameraControllerFuture;
 
-      final path = join((await getTemporaryDirectory()).path, '${DateTime.now()}.png');
+      final path =
+      join((await getTemporaryDirectory()).path, '${DateTime.now()}.png');
       await _cameraController.takePicture(path);
-      Navigator.pop(context,path);
-
+      Navigator.pop(context, path);
     } catch (e) {
       print(e);
     }
@@ -277,4 +321,83 @@ class _TakePictureState extends State<TakePicture> {
     _cameraController.dispose();
     super.dispose();
   }
+}
+
+
+
+
+
+// -------------------------------------------------------------------
+// -------- Show Firebase data --------
+class FireBasePicture extends StatefulWidget {
+  @override
+  _FireBasePictureState createState() {
+    return _FireBasePictureState();
+  }
+}
+
+class _FireBasePictureState extends State<FireBasePicture> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Firebase Pictures'), backgroundColor: Colors.blue[900]),
+      body: _table(context),
+    );
+  }
+
+  Widget _table(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('dbphotos').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+
+        return _rows(context, snapshot.data.documents);
+      },
+    );
+  }
+
+  Widget _rows(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _onePicture(context, data)).toList(),
+    );
+  }
+
+  Widget _onePicture(BuildContext context, DocumentSnapshot data) {
+    final onepic = OnePicture.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(onepic.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(onepic.name + "\n" + onepic.value.toString().substring(0,100) + "..." ),
+          trailing: Text('jSON'),   //Text(onepic.value)
+          onTap: () => {},
+        ),
+      ),
+    );
+  }
+}
+
+class OnePicture {
+  final String name;
+  final String value;
+  final DocumentReference reference;
+
+  OnePicture.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['value'] != null),
+        name = map['name'],
+        value = map['value'];
+
+  OnePicture.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$name:$value>";
 }
